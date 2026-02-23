4f44833ca32b48d8716b512fd4198bcd1e4aec24
Dvizhok <kalibry1@gmail.com>
2026-02-23 11:24:11 +0200

    1

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,6 +4,7 @@ import axios from "axios";
 import { useState } from "react";
 import { type Article } from "../../types/article";
 import ArticleList from "../ArticleList/ArticleList";
+import { Bars } from "react-loader-spinner";
 
 interface ArticlesHttpResponse {
   hits: Article[];
@@ -11,18 +12,40 @@ interface ArticlesHttpResponse {
 
 export default function App() {
   const [articles, setArticles] = useState<Article[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const handleSearch = async (topic: string) => {
-    const response = await axios.get<ArticlesHttpResponse>(
-      `https://hn.algolia.com/api/v1/search?query=${topic}`,
-    );
-    console.log(response.data.hits);
-    setArticles(response.data.hits);
+    try {
+      setIsLoading(true);
+      setIsError(false);
+      const response = await axios.get<ArticlesHttpResponse>(
+        `https://hn.algolia.com/api/v1/search?query=${topic}`,
+      );
+      setArticles(response.data.hits);
+      console.log(response.data.hits);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
     <>
       <SearchForm onSubmit={handleSearch} />
+      {isLoading && (
+        <Bars
+          height="30"
+          width="30"
+          color="#2b9ace"
+          ariaLabel="bars-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      )}
+      {isError && <p>Whoops, something went wrong! Please try again!</p>}
       {articles.length > 0 && <ArticleList items={articles} />}
     </>
   );
